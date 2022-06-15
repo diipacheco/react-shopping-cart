438d3bc8a68d054cc83ffa85bb24711211867c53
diipacheco <edilsonpacheco6@gmail.com>
2022-06-15 19:55:28 -0300

    adds styled-components and global application styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
-import React from "react";
+import React from 'react';
+import GlobalStyles from './styles/GlobalStyles';
 
 function App() {
   return (
-    <div className="App">
-      <h1>hello world</h1>
-    </div>
+    <>
+      <GlobalStyles />
+      <div className="App">
+        <h1>hello world</h1>
+      </div>
+    </>
   );
 }
 
---
+++ src/styles/GlobalStyles.tsx
@@ -0,0 +1,33 @@
+import { createGlobalStyle } from 'styled-components';
+
+export default createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    color: var(--dark-grey);
+  }
+  html, body, #root {
+    max-width: 100vw;
+    max-height: 100vh;
+    width: 100%;
+    height: 100%;
+  }
+  *, button, input {
+    border: 0;
+    background: none;
+    font-family: 'Roboto', sans-serif;
+  }
+  html {
+    background: var(--background);
+  }
+  :root {
+    --white: #FFFFFF;
+    --brown: #47250E;
+    --light-brown: #786050;
+    --orange: #F78131;
+    --dark-orange: #C46627;
+    --background: #E6CAA7;
+    --box-shadow: #00000029;
+  }
+`;
